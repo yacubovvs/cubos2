b57bcba76917252bdf3a0fe1ab4c48904f108c36
Vitaly <yacubovvs@yandex.ru>
2020-07-27 08:55:56 +0300

    new ImagePanel fix

--- Server/src/ru/cubos/connectors/emulator/ImagePanel.java
+++ Server/src/ru/cubos/connectors/emulator/ImagePanel.java
@@ -10,6 +10,10 @@ public class ImagePanel extends JPanel {
     private BufferedImage image;
     private JPanel imageWrapper;
 
+    float image_k = 1;
+    boolean vertivalOffset = false;
+    int offsetSize = 0;
+
     public ImagePanel() {
     }
 
@@ -28,12 +32,16 @@ public class ImagePanel extends JPanel {
         if(image != null){
 
             float panel_k = (float)getWidth()/(float)getHeight();
-            float image_k = (float)image.getWidth()/(float)image.getHeight();
+            image_k = (float)image.getWidth()/(float)image.getHeight();
 
             if(panel_k>image_k){
-                g.drawImage(image,  (getWidth() - (int)(getHeight()*image_k))/2, 0, (int)(getHeight()*image_k), getHeight(), null);
+                vertivalOffset = false;
+                offsetSize = (getWidth() - (int)(getHeight()*image_k))/2;
+                g.drawImage(image,  offsetSize, 0, (int)(getHeight()*image_k), getHeight(), null);
             }else{
-                g.drawImage(image, 0, (getHeight() - (int)(getWidth()/image_k))/2, getWidth(),  (int)(getWidth()/image_k), null);
+                vertivalOffset = true;
+                offsetSize = (getHeight() - (int)(getWidth()/image_k))/2;
+                g.drawImage(image, 0, offsetSize, getWidth(),  (int)(getWidth()/image_k), null);
             }
 
         }
