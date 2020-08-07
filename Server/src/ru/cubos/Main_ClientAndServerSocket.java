3669d3ea2d9956968e05d763ac0421c27f5fe037
yacubovvs <yacubovvs@yandex.ru>
2020-08-08 01:06:31 +0300

    IP adress fixed in socket client

--- Server/src/ru/cubos/Main_ClientAndServerSocket.java
+++ Server/src/ru/cubos/Main_ClientAndServerSocket.java
@@ -1,9 +1,17 @@
 package ru.cubos;
 
+import java.net.UnknownHostException;
+
 public class Main_ClientAndServerSocket {
     public static void main(String[] args) {
         Thread serverThread = new Thread(() -> Main_SocketServer.main(args));
-        Thread clientThread = new Thread(() -> Main_SocketClient.main(args));
+        Thread clientThread = new Thread(() -> {
+            try {
+                Main_SocketClient.main(args);
+            } catch (UnknownHostException e) {
+                System.out.println("Client socket start error");
+            }
+        });
 
         serverThread.start();
         clientThread.start();
--- Server/src/ru/cubos/Main_SocketClient.java
+++ Server/src/ru/cubos/Main_SocketClient.java
@@ -2,13 +2,13 @@ package ru.cubos;
 
 import ru.cubos.connectors.socketEmulatorClient.SocketClient;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 public class Main_SocketClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         SocketClient socketClient = new SocketClient(1080/3, 2340/3);
-        socketClient.start("192.168.1.38", 8000);
-        //Server server = new Server(socketClient);
-        //socketClient.start("10.0.0.153", 8000);
-        //server.setRepaintPending();
+        socketClient.start(Inet4Address.getLocalHost().getHostAddress(), 8000);
     }
 
 
