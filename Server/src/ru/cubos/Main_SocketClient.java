3a33eb3eda8978900fdb6bd74cca656f071ce132
Vitaly <yacubovvs@yandex.ru>
2020-08-11 01:01:25 +0300

    added init server address

--- Server/src/ru/cubos/Main_SocketClient.java
+++ Server/src/ru/cubos/Main_SocketClient.java
@@ -7,11 +7,12 @@ import java.net.UnknownHostException;
 
 import static ru.cubos.commonHelpers.StaticScreenSettings.screenHeight_init;
 import static ru.cubos.commonHelpers.StaticScreenSettings.screenWidth_init;
+import static ru.cubos.commonHelpers.StaticSocketSettings.cosketServerADdress;
 
 public class Main_SocketClient {
     public static void main(String[] args) throws UnknownHostException {
         SocketClient socketClient = new SocketClient(screenWidth_init, screenHeight_init);
-        socketClient.start(Inet4Address.getLocalHost().getHostAddress(), 8000);
+        socketClient.start(cosketServerADdress, 8000);
         //socketClient.start("192.168.1.38", 8000);
     }
 
--- Server/src/ru/cubos/commonHelpers/StaticSocketSettings.java
+++ Server/src/ru/cubos/commonHelpers/StaticSocketSettings.java
@@ -6,4 +6,6 @@ public class StaticSocketSettings {
 
     public static int clientBufferSize_max = 16 * 1024 * 1024;
     public static int serverBufferSize_max = 16 * 1024 * 1024;
+
+    public static String cosketServerADdress = "192.168.1.38"; // Inet4Address.getLocalHost().getHostAddress()
 }
