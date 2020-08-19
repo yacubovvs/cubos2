835127ef2c75a81630aaee004349d2ca8d3b236c
Vitaly <yacubovvs@yandex.ru>
2020-08-19 18:05:34 +0300

    added new device for serial port client

--- Clients/ArduinoCubOsClient/ArduinoCubOsClient.ino
+++ Clients/ArduinoCubOsClient/ArduinoCubOsClient.ino
@@ -6,12 +6,19 @@
 ########################################################
 */
 
+// CPU
+//#define esp32
+#define mega256
+
 // DISPLAY
-#define display_M5Stack
+//#define display_M5Stack
+#define megaUnoShield
+#define megaUnoShield_2_8_inc
+
 
 // CONNECTOR TYPE
-//#define connector_serial
-#define connector_wifi_socket_server
+#define connector_serial
+//#define connector_wifi_socket_server
 
 //#define ssid "DIR-615"
 //#define password "tsdurovo6200"
@@ -25,13 +32,19 @@ char * password = "tsdurobo6200";
 #                                                      #
 ########################################################
 */
-#include <M5Stack.h>
+#ifdef esp32
+  #include <M5Stack.h>
+#endif
 
 void setup() {
-  M5.begin();
+  #ifdef esp32
+    M5.begin();
+  #endif
+  
   setup_displayDriver();
+  
   #ifdef connector_serial
-    Serial.begin(115200);
+    Serial.begin(9600);
   #endif
 
   #ifdef connector_wifi_socket_server
@@ -46,8 +59,12 @@ void loop() {
   //drawRect(40, 10, 70,  40,    0,   255,     0,  true);
   //drawRect(70, 10, 100, 40,    0,     0,   255,  true);
 
+  drawLine(20,20,50,50,255,0,0);
+
   #ifdef connector_serial
-    Serial.println("Test " + millis());
+    drawString("Test!!!", 10,10,255,0,0,1);
+    Serial.println("Test!!!");
+    delay(1000);
   #endif
 
   #ifdef connector_wifi_socket_server
---
+++ Clients/ArduinoCubOsClient/display_driver_megaUnoShield_2_8_inc.ino
@@ -0,0 +1,54 @@
+#ifdef megaUnoShield
+  #ifdef megaUnoShield_2_8_inc
+    #define SCREEN_WIDTH 240
+    #define SCREEN_HEIGHT 320
+  #else
+    #define SCREEN_WIDTH 320
+    #define SCREEN_HEIGHT 480
+  #endif
+  
+  //#include "libs_h/MCUFRIEND_kbv/UTFTGLUE.h"              
+  #include <UTFTGLUE.h>              
+  //#include <MCUFRIEND_kbv.h>              
+  //#include "libs_h/MCUFRIEND_kbv/UTFTGLUE.h"              
+  UTFTGLUE myGLCD(0,A2,A1,A3,A4,A0);
+  
+  byte red = 0, green = 0, blue = 0;
+  
+  void setup_displayDriver(){
+    myGLCD.InitLCD();
+    //myGLCD.setFont(SmallFont);
+    //myGLCD.wrap(false)
+    myGLCD.setRotation(PORTRAIT);
+  
+    //myGLCD.clrScr();
+    setDrawColor(0, 0, 0);
+    myGLCD.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);  
+
+    
+    //drawString("Test ", 10,10,255,255,255,1);
+  }
+
+  void setDrawColor(byte red_set, byte green_set, byte blue_set){
+    red = red_set;
+    green = green_set; 
+    blue = blue_set;
+    
+    myGLCD.setColor(red, green, blue);
+  }
+  
+  void drawPixel(int x, int y, byte red_set, byte green_set, byte blue_set){
+
+    /*
+    if(
+      red_set != red ||
+      green_set != green ||
+      blue_set != blue
+      ) setDrawColor(red, green, blue);*/
+
+    setDrawColor(0, 255, 0);
+    //setDrawColor(red, green, blue);
+    myGLCD.drawPixel(x,y);
+  }
+  
+#endif
